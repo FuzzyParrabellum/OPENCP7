760c93ebf24c0729f98d2f5f925b0720fe952c74
notRoot <kalimba@pc-34.home>
2021-12-09 12:52:31 +0100

    A rendu un 1er jet pour l'exam du 10 decembre, doit maintenant retravailler un peu algo Optimized.py ainsi que les slides du projet

--- OptimizedV2.py
+++ OptimizedV2.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python
-# -*- coding: latin-1 -*-
+# -*- coding: utf-8 -*-
 """
 Algorithme permettant de maximiser le profit des clients après 2 ans
 - Chaque action ne peut être achetée qu'une fois
@@ -7,16 +7,7 @@ Algorithme permettant de maximiser le profit des clients après 2 ans
 - On ne peut dépenser au maximum que 500 euros par client
 
 On veut ici trouver une version plus rapide que bruteforce.py pour le même problème,
-on a pour l'instant choisi d'implémenter un algorithme PSE branch and bound avec 
-possiblement un algorithme glouton afin d'aider à calculer le "poids" des branches.
-
-Voit pour l'instant le cours d'optimisation discrète de Melbourne disponible sur Coursera.
-N'est peut-être pas le plus indiqué ici étant donné qu'on a pas vraiment deux séries de valeurs
-différentes qui ne peuvent pas se mélanger, comme le poids et le prix d'un objet, ici avec les actions
-et leur pourcentage ont a plutôt une seule valeur. Ou pas. On a d'un coté le prix qui correspond au
-poids global et de l'autre coté le pourcentageXce prix qui donne la valeur future de cet achat. C'est
-cette valeur là qu'on essaie de maximiser, en essayant de ne pas dépasser le poids, et donc le prix
-limite.
+on a pour l'instant choisi d'implémenter un algorithme glouton pour se faire.
 
 """
 
@@ -25,15 +16,18 @@ import csv
 import time
 
 
-# Lire les informations du fichier csv
+# Lecture des informations du fichier csv
 cost_list = []
 benefit_list = []
 action_names = []
 
 profit_format = ''
 
+# On indique ici le fichier dataset à ouvrir
+# file_to_open = './dataset2_Python+P7.csv'
 # file_to_open = './dataset1_Python+P7.csv'
 file_to_open = './InvestInformation.csv'
+
 
 if file_to_open == './InvestInformation.csv':
     print("The profit is written as percentage")
@@ -71,22 +65,35 @@ else:
                     line_count += 1
 
 
+
 def index_exists(list, index):
+    """Fonction permettant de vérifier qu'un index existe bien"""
     if index < len(list):
         return True
     else:
         return False
 
+def calculate_investment_return(combination, format):
+    total = 0
+    if format == 'percentage':
+        for index in combination:
+            total += (cost_list[index]*benefit_list[index])
+    else:
+        for index in combination:
+            total += (benefit_list[index]/cost_list[index])
+    return total
 
 def Gloutonic(list1, list2, format, max_capacity):
+    """Fonction principale utilisant un algorithme glouton afin de trier les actions suivant
+    un rapport coût/bénéfice pour ensuite remplir un portefeuille d'action avec les actions
+    présentant les meilleurs rapports sans dépasser un coût d'achat maximal.
+    list1 correspond aux coûts, list2 correspond aux bénéfices, format est le format des bénéfices
+    (pourcentage ou autre), max_capacity est le coût maximal à ne pas dépasser pour remplir le
+    portefeuille d'actions.
+    """
     new_list = []
-    # On veut remplir new_list avec la liste d'actions optimales
-    # pour se faire on doit faire un rapport de cout/weight pour chaque action
-    # trier ensuite tous ses rapports avec le plus fort en 1er
-
     # 1ere phase de gloutonic ou on sort les actions par leur rapport cout*intéret
     for index in range(len(list1)):
-        # Pour chaque index du nombre d'éléments dans la list d'actions
         if list1[index] == 0:
             continue
 
@@ -100,24 +107,18 @@ def Gloutonic(list1, list2, format, max_capacity):
             new_list.append(rapport)
 
         else:
-            # cas 1 l'élément est plus grand ou égal que le dernier élément de la liste
             if new_list[-1][0] <= rapport[0]:
                 new_list.append(rapport)
-            # cas 2 l'element est plus petit ou egal que le 1er élément de la liste
+
             elif new_list[0][0] >= rapport[0]:
                 new_list = [rapport] + new_list
+
             else:
-                # cas 3 l'élément est entre les deux
-                # dans ce cas, pour chaque élément de la liste on voit si il est sup ou inf à         # dans ce cas, pour chaque élément de la liste on voit si il est sup ou inf à celui d'après
-                # ou d'avant celui d'après ou d'avant
                 for element_index in range(len(new_list)):
                     if index_exists(new_list, element_index+1):
                         if new_list[element_index][0] <= rapport[0] <= new_list[element_index+1][0]:
                             new_list.insert(element_index+1, rapport)
                             break
-                    # si notre rapport se situe entre un élément et l'élément d'après, alors
-                    # si on est en fin de liste, pas besoin de continuer et faire index_exists
-    # print(f'La new_list finale est égale à {new_list}')
 
     # 2e phase, on va maintenant remplir notre portefeuille d'actions en commençant par les actions
     # ayant le meilleur indice (pour se faire on va retourner la liste précédente) et en voyant 
@@ -127,57 +128,30 @@ def Gloutonic(list1, list2, format, max_capacity):
     actions_bought = []
     for action in action_list:
         if current_capacity + action[2] <= max_capacity:
-            # print(f'current_capacity={current_capacity}')
-            # print(f'actions[1]={action[1]}')
             current_capacity += action[2]
             actions_bought.append(action[1])
 
+    total_profit = calculate_investment_return(actions_bought, format)
+
     if format == 'percentage':
-        return print(f'The current_capacity is {current_capacity} for the combination of actions {actions_bought}')
+        return current_capacity, actions_bought, total_profit
     
     else:
         names_list = []
         for action in actions_bought:
             names_list.append(action_names[action])
         
-        return print(f'The current_capacity is {current_capacity} for the combination of actions {names_list}')
-
-    # important, on doit ajouter +2 à chaque index pour pouvoir lire ceux-ci directement
-    # comme on doit suppr les deux premières lignes du fichier csv
-    # for index in range(len(actions_bought)):
-    #     actions_bought[index] += 2
-
-    # return print(f'The current_capacity is {current_capacity} for the actions_indexes {actions_bought} \
-    #             and the names {names_list}')
-
-
-
-        
+        return current_capacity, names_list, total_profit
 
 
 
 def main():
-    Gloutonic(cost_list, benefit_list, profit_format, 500)
+    current_cap, names, profit = Gloutonic(cost_list, benefit_list, profit_format, 500)
+    print(f'''The current_capacity is {current_cap} for the combination of actions {names}
+    with a return of {profit}''')
 
 start = time.perf_counter()
 main()
 end = time.perf_counter()
 
-print(f"Le temps d'exécution de la fonction OptimizedV2.py est de {end - start}")
-
-"""
-téléch template google slide
-mettre titres sur chaque slide
-numéroter les pages et mettre sur combien de pages
-
-la complexité est déjà un ordre de grandeur
-
-complex temporelle temps d'éxecution
-
-montrer les recherches déjà faites sur les algorithmes optimisés
-pouvoir expliquer pourquoi avoir avoir choisi l'algo glouton en particulier
-
-important mettre le comparatif a la fin
-
-mettre juste quelques lignes de commentaires en-dessous de chaque fonction et supprimer
-tous les autres commentaires"""
+print(f"Le temps d'exécution de la fonction OptimizedV2.py est de {end - start}s")
--- bruteforce3.py
+++ bruteforce3.py
@@ -1,3 +1,5 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
 """
 Algorithme permettant de maximiser le profit des clients après 2 ans
 - Chaque action ne peut être achetée qu'une fois
@@ -24,47 +26,28 @@ with open('./InvestInformation.csv') as csv_file:
     line_count = 0
     for row in csv_reader:
         if line_count == 0:
-            # print(f'Column names are {", ".join(row)}')
             line_count += 1
         else:
-            print(f'\t{row[0]} euros of cost for {row[1]} of benefit')
+            # print(f'\t{row[0]} euros of cost for {row[1]} of benefit')
             cost_list.append(int(row[0]))
             benefit_list.append(float(row[1].replace(",", ".")))
             line_count += 1
-    # print(f'Processed {line_count} lines.')
+    
 
 
 def index_exists(list, index):
+    """Fonction permettant de vérifier qu'un index existe bien"""
     if index < len(list):
         return True
     else:
         return False
 
 def return_best(actual_best, possible_best, list_of_costs, list_of_profits, max):
-    # On compare la meilleure combinaison trouvée jusqu'à présent avec une potentielle
-    # meilleure combinaison, elle sera meilleure si elle ne dépasse pas la valeur 
-    # d'achat maximum et si son profit est supérieur à la meilleure actuelle.
+    """On compare la meilleure combinaison trouvée jusqu'à présent avec une potentielle
+    meilleure combinaison, elle sera meilleure si elle ne dépasse pas la valeur 
+    d'achat maximum et si son profit est supérieur à la meilleure actuelle."""
     possible_amount = 0
     possible_profit = 0
-    # print(f'possible_best = {possible_best}')
-
-    # On vérifie si possible_best est une combinaison d'index d'actions ou un seul index auquel cas 
-    # on obtiendra un type 'int'
-    # if isinstance(possible_best, int) == True:
-    #     possible_amount += list_of_costs[possible_best]
-    #     possible_profit += list_of_profits[possible_best]*list_of_costs[possible_best]
-    #     if possible_amount > max:
-    #         return actual_best
-
-    #     else:
-    #         actual_best_amount = actual_best[1]
-    #         actual_best_profit = actual_best[2]
-
-    #         if actual_best_amount > max or actual_best_profit < possible_profit:
-    #             return [possible_best, possible_amount, possible_profit]
-
-    #         else:
-    #             return actual_best
 
     for i in possible_best:
         possible_amount += list_of_costs[i]
@@ -86,13 +69,12 @@ def return_best(actual_best, possible_best, list_of_costs, list_of_profits, max)
 
 
 def take_first_combination(index, list_of_costs, list_of_profits):  
-    
+    """La fonction take_first_combination sert à return une première combinaison,
+    avec son coût et son profit. Cette combinaison comportera toutes les actions
+    disponibles."""
     original_combination = []
     current_amount = 0
     current_profit = 0
-    # Si il existe encore une action après celle actuelle, et que la rajouter à notre combinaison ne ferait pas dépasser
-    # notre montant d'achat du montant maximum, alors on la rajoute à la combinaison ainsi que son coût au montant d'achat
-    # actuel
     for action_number in range(len(list_of_costs)):
         # la condition ci-dessous permet de ne pas rajouter les index déjà explorés par les précédents
         # appels de add_combinations
@@ -114,8 +96,7 @@ def calculate_investment_return(combination):
         
 def add_combinations(index, list_of_costs, list_of_profits, max_amount,\
                      best_combination = []):
-    print("add_combinations est appelé")
-    print(f"à la boucle n° {index}, la best_combination est {best_combination}")
+    
     # à partir d'un index d'une liste donnée, on va chercher toutes les combinaisons possibles
     # à partir du reste des index de cette liste, ces combinaisons ne devant pas dépasser le maximum
     # autorisé d'achat symbolisé par la variable max_amount. 
@@ -129,7 +110,7 @@ def add_combinations(index, list_of_costs, list_of_profits, max_amount,\
     original_combination, current_amount, current_profit = take_first_combination(index,\
                                                          list_of_costs, list_of_profits)                                                  
     current_best = [original_combination, current_amount, current_profit]
-    print(f"L'original_combination est de {original_combination}")
+    print(f"On cherche les combinaisons à partir de l'index {index} et de {original_combination}")
     # La condition ci-dessous sert à déterminer si add_combinations a déjà été apellée et permet
     # de check quelle est la meilleure coombinaison entre les appels de add_combination précédents et
     # l'original_combination de notre add_combination actuelle.
@@ -144,8 +125,6 @@ def add_combinations(index, list_of_costs, list_of_profits, max_amount,\
     # on renregistre, puis l'avant-avant
     # dernier etc. jusqu'à ce qu'on arrive à l'index d'où part notre combinaison
 
-    #@doit maintenant trouver un moyen d'executer la deuxieme partie du plan, récursion?
-
     # Pour chaque élément de notre original_combination - 1, on va effectuer la fonction A
     # BOUCLE FONCTION A qu'on effectue len(original_combination) - 1:
         # On ne rajoute aucune des deux dernières valeurs 
@@ -158,7 +137,6 @@ def add_combinations(index, list_of_costs, list_of_profits, max_amount,\
     before_stop = 0
     combinations = []
     for element in range(len(original_combination) - 1):
-        # print(f"Juste après le element, combinations={combinations}")
         before_stop += 1
         current_list = []
         if element == 0:
@@ -166,13 +144,11 @@ def add_combinations(index, list_of_costs, list_of_profits, max_amount,\
             current_list.append([action_index[-1]])
             current_list.append([action_index[-2]])
             current_list.append([action_index[-1], action_index[-2]])
-            # print(f'Au premier tour current_list = {current_list}')
             combinations = combinations + current_list
             for combination in current_list:
                 current_best = return_best(current_best, combination, list_of_costs, \
                 list_of_profits, max_amount) 
-            # print(f'Et combinations = {combinations}')
-        # autrement truc
+            
         else:
             index_to_add = element + 2
         #         # C'est à dire juste après notre 1ere condition
@@ -198,78 +174,6 @@ def add_combinations(index, list_of_costs, list_of_profits, max_amount,\
             # combinations total
             combinations = current_list + combinations
 
-        # if before_stop == 5:
-        #     hello = input('what next?')
-        #     print(hello)
-
-
-        # if len(current_list) == 0:
-        #     # alors rien, c'est le premier truc
-        #     current_list.append(action_index[-1])
-        #     current_list.append(action_index[-2])
-        #     current_list.append([action_index[-1], action_index[-2]])
-        # # autrement truc
-        # else:
-        #     if len(current_list) == 3:
-        #         # C'est à dire juste après notre 1ere condition
-        #         current_list.append(action_index[-3])
-        #         for combination in current_list:
-        #             current_list.append(action_index[-3] + combination)
-        #     else:
-        #         # le troisième cas quand on a la suite
-        #         # il faudrait rendre ce 3e cas itérable peu importe le nombre d'actions et d'index,
-        #         # et donc écrire autre chose que current_list.append(action_index[-4] + combination)
-        #         current_list.append(action_index[-4])
-        #         for combination in current_list:
-        #             current_list.append(action_index[-4] + combination)
-
-        # normalement à partir d'ici, devrait avoir le bon current_best et peut donc lancer le code
-        # pour voir ce que ça donne.
-        
-
-        # Ici il faut faire une comparaison de chaque combinaison que l'on trouve avec notre
-        # current best
-        # Est-ce qu'il ne faudrait pas avoir bcp plus de combinaison / de boucles ?
-
-        # pourrait supprimer current_list pour juste le remplacer par combinations, ou alors sortir
-        # current_list d'un niveau et supprimer combinations, je pense que c'est redondant d'avoir
-        # les deux.
-
-    
-
-
-    # pour chaque action entre la 1ere et la derniere action, on va effectuer la boucle qu'on a déjà
-    # construite. A la fin d'une boucle, on va juste commencer celle-ci un avant-dernier item plus
-    # près.
-    # if len(original_copy) > 2:
-    #     while len(original_copy) > 2:
-    #         print("while loop")
-    #         for item in range(len(original_copy)-2):
-    #             copy_throwable = original_copy[:]
-    #             copy_length = len(copy_throwable)
-
-    #             last_index_starter = 2
-    #             for action in range(copy_length):
-    #                 if len(copy_throwable) <= last_index_starter:
-    #                     # print("top")
-    #                     break
-    #                 else:
-    #                     print(len(copy_throwable))
-    #                     copy_throwable.pop(-last_index_starter)
-    #                     copy_2 = copy_throwable[:]
-    #                     current_best = return_best(current_best, copy_2, list_of_costs, \
-    #                         list_of_profits, max_amount)
-    #                     # print(current_best)
-    #             last_index_starter += 1
-    #         original_copy.pop()
-    # else:
-    #     pass
-
-
-    # En gardant notre dernier-index ET celui d'avant, on fait la même chose etc.
-    # Une fois fini ces deux boucles,
-    # on pop le tout dernier index et on recommence les deux boucles
-
 
     return current_best
 
@@ -278,26 +182,18 @@ max_amount = 500
 best_combination = []
 current_amount = 0
 
+
 start = time.perf_counter()
 for index in range(len(cost_list)):
     combination_start = [index]
-    
     best_combination = add_combinations(index, cost_list, benefit_list, max_amount, best_combination)
-   
-    
-    print("ON A ATTEINT LA FIN DE LA BOUCLE GLOBALE n°" + str(index+1))
-    print(f"Pour l'instant le meilleur portefeuille d'actions est {best_combination}")
-    print(f"Son return est de {calculate_investment_return(best_combination[0])}\n")
 
-
-print(f"Le meilleur portefeuille d'actions est {best_combination}")
+print(f"Au final, Le meilleur portefeuille d'actions est {best_combination[0]}")
 print(f"Son return est de {calculate_investment_return(best_combination[0])}")
-
+print(f"Pour un coût total de {best_combination[1]}")
 end = time.perf_counter()
-print(f"Le temps d'exécution de la fonction bruteforce3.py est de {end - start}")
+print(f"Le temps d'exécution de la fonction bruteforce3.py est de {end - start}s")
 
-# Afficher le résultat
-# main()
 
 
 
